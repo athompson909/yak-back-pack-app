b6172830a8605d3c8f744d24ce2e41a3945d0b0d
athompson909 <athompson909@gmail.com>
2016-10-01 21:23:23 -0600

    added videofragment

---
+++ app/src/main/java/com/at/myapplication/main/base/VideoFragment.java
@@ -0,0 +1,17 @@
+package com.at.myapplication.main.base;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+/**
+ * Created by adamthompson on 10/1/16.
+ */
+public class VideoFragment extends Fragment {
+
+    private VideoView videoView;
+
+    // inflate to view
+
+    // start video
+}
