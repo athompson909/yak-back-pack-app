5852c1e0093659d5d5f8724e84cd77ff1dce84be
athompson909 <athompson909@gmail.com>
2016-10-06 20:57:14 -0600

    video stuff

--- app/src/main/java/com/at/myapplication/main/base/VideoFragment.java
+++ app/src/main/java/com/at/myapplication/main/base/VideoFragment.java
@@ -2,7 +2,13 @@ package com.at.myapplication.main.base;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.VideoView;
+
+import com.at.myapplication.R;
 
 /**
  * Created by adamthompson on 10/1/16.
@@ -10,6 +16,18 @@ import android.widget.VideoView;
 public class VideoFragment extends Fragment {
 
     private VideoView videoView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_video, container, false);
+
+        // todo: start video
+
+        VideoView videoView = (VideoView) v.findViewById(R.id.video);
+
+        return v;
+    }
 
     // inflate to view
 
