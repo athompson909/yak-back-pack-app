eb56217ab415ecb77fa20b0071fa708b64cf784d
athompson909 <athompson909@gmail.com>
2016-10-06 21:10:19 -0600

    video stuff

--- app/src/main/java/com/at/myapplication/main/base/VideoFragment.java
+++ app/src/main/java/com/at/myapplication/main/base/VideoFragment.java
@@ -1,11 +1,13 @@
 package com.at.myapplication.main.base;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.at.myapplication.R;
@@ -23,6 +25,13 @@ public class VideoFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_video, container, false);
 
         // todo: start video
+
+        VideoView video = (VideoView) v.findViewById(R.id.video);
+        video.setVideoURI(Uri.parse("android.resource://" + v.getContext().getPackageName()+ "/"+R.raw.car));
+        video.setMediaController(new MediaController(v.getContext()));
+        video.requestFocus();
+        video.start();
+
 
         VideoView videoView = (VideoView) v.findViewById(R.id.video);
 
